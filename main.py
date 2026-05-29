7205367811560e8d036fda53784036cc68ee2b19
Romain PATUREAU <romain.patureau@outlook.com>
2026-05-29 16:20:16 +0200

    Cog start on message

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import os
 from dotenv import load_dotenv
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN').strip()
@@ -12,12 +12,33 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f'We have logged in as {bot.user}')
-    
-@bot.command()
-async def ping(ctx):
-    await ctx.send('Pong!')
 
+class MyCog(commands.Cog):
+    def __init__(self, bot):
+        self.index = 0
+        self.bot = bot
+    
+    @bot.event
+    async def on_ready():
+        print(f'Logged in as {bot.user}')
+
+    @bot.event
+    async def on_message(message):
+        if message.author == bot.user:
+            return
+        if message.content.startswith('!start'):
+            print("Starting the printing task...")
+            my_cog = MyCog(bot)
+            my_cog.printing.start()
+
+    @tasks.loop(seconds=5.0)
+    async def printing(self):
+        print(self.index)
+        self.index += 1
+    
+    @printing.before_loop
+    async def before_printing(self):
+        print('Waiting until the bot is ready...')
+        await self.bot.wait_until_ready()
+    
 bot.run(TOKEN)
